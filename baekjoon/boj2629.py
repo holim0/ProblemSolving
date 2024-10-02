d0f6438e47b96f457a7009a8794f0017a57c594a
holim0 <holim1226@gmail.com>
2024-10-02 23:30:54 +0900

    문제 풀이 반영

--- baekjoon/boj2629.py
+++ baekjoon/boj2629.py
@@ -7,9 +7,21 @@ m = int(input())
 
 target = list(map(int, input().split()))
 
-dp = [False for _ in range(40001)]
+dp = [[False] *40001 for _ in range(n)]
 
-dp[0] = True
+dp[0][0] = True
 
 for i in range(n):
+    dp[i][info[i]] = True
+    for w in range(1, w_sum+1):
+        if dp[i-1][w]:
+            dp[i][w] = True
+            dp[i][info[i]+w] = True
+            dp[i][abs(info[i]-w)] = True
+    
 
+for t in target:
+    if dp[n-1][t]:
+        print("Y", end=" ")
+    else:
+        print("N", end= " ")
